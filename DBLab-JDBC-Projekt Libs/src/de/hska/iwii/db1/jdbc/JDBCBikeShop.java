416be1cacd5928d6ecfed060fce3373880905159
Bjoern <malzacher.bjoern@gmail.com>
2023-05-17 10:04:43 +0200

    lol

---
+++ DBLab-JDBC-Projekt Libs/src/de/hska/iwii/db1/jdbc/JDBCBikeShop.java
@@ -0,0 +1,89 @@
+package de.hska.iwii.db1.jdbc;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
+/**
+ * Diese Klasse ist die Basis für Ihre Lösung. Mit Hilfe der
+ * Methode reInitializeDB können Sie die beim Testen veränderte
+ * Datenbank wiederherstellen.
+ */
+public class JDBCBikeShop {
+    public static void main(String[] args) {
+        try {
+            new JDBCBikeShop().createConnection("admin", "admin", "db_1-postgres-1", 5432, "", "");
+        } catch (SQLException e) {
+          System.out.println("didn't work");
+            e.printStackTrace();
+        }
+    }
+    public void createConnection(String user,String pw, String server, int port, String db, String scheme  ) throws SQLException{
+        try {
+            Class.forName("org.postgresql.Driver" );
+            String serverName = "localhost";
+            String mydatabase = "postgres";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase; 
+        
+            String username = "admin";
+            String password = "admin";
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+
+
+        } catch (ClassNotFoundException e) {
+            
+            e.printStackTrace();
+        }  
+    }
+    /**
+     * Stellt die Datenbank aus der SQL-Datei wieder her.
+     * - Alle Tabllen mit Inhalt ohne Nachfrage löschen.
+     * - Alle Tabellen wiederherstellen.
+     * - Tabellen mit Daten füllen.
+     * <p>
+     * Getestet mit MsSQL 12, MySql 8.0.8, Oracle 11g, Oracle 18 XE, PostgreSQL 14.
+     * <p>
+     * Das entsprechende Sql-Skript befindet sich im Ordner ./sql im Projekt.
+     * @param connection Geöffnete Verbindung zu dem DBMS, auf dem die
+     * 					Bike-Datenbank wiederhergestellt werden soll. 
+     */
+    public void reInitializeDB(Connection connection) {
+        try {
+            System.out.println("\nInitializing DB.");
+            connection.setAutoCommit(true);
+            String productName = connection.getMetaData().getDatabaseProductName();
+            boolean isMsSql = productName.equals("Microsoft SQL Server");
+            Statement statement = connection.createStatement();
+            int numStmts = 0;
+            
+            // Liest den Inhalt der Datei ein.
+            String[] fileContents = new String(Files.readAllBytes(Paths.get("sql/Bike.sql")),
+					StandardCharsets.UTF_8).split(";");
+            
+            for (String sqlString : fileContents) {
+                try {
+                	// Microsoft kenn den DATE-Operator nicht.
+                    if (isMsSql) {
+                        sqlString = sqlString.replace(", DATE '", ", '");
+                    }
+                    statement.execute(sqlString);
+                    System.out.print((++numStmts % 80 == 0 ? "/\n" : "."));
+                } catch (SQLException e) {
+                    System.out.print("\n" + sqlString.replace('\n', ' ').trim() + ": ");
+                    System.out.println(e.getMessage());
+                }
+            }
+            statement.close();
+            System.out.println("\nBike database is reinitialized on " + productName +
+                    "\nat URL " + connection.getMetaData().getURL()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
